00ca1c8e39f4a15f11b8e4221c97906e89d07a1d
Ronit shahu <67621176+Devilreaper123@users.noreply.github.com>
2022-09-14 11:46:31 +0530

    RESTful

--- app.py
+++ app.py
@@ -1,72 +1,37 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT,jwt_required
 
+from security import authenticate, identity
 app = Flask(__name__)
+app.secret_key = 'ronit'
+api = Api(app)
+items = []
+jwt = JWT(app , authenticate , identity) # Creates a new endpoint named /auth
 
-stores = [
-{
-    'name':'My Wonderful Store',
-    'items': [
-        {
-            'name':'My Item',
-            'price':15.99
-        }
-    ]
-}
-]
-# Home
-@app.route('/')
-def home():
-    return render_template('index.html')
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {"Item": item}, 200 if item else 404
 
-# POST /store data: {name:}
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name':request_data['name'],
-        'item':[]
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+    def post(self, name):
+        # force=True(Converts the data into json, does not look for the content type) , silent=True (Does not give error just return none )
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {"Message": f"An item with the name '{name}' already exist"}, 400
+        data = request.get_json()
+        item = {"name": name, "price": data['price']}
+        items.append(item)
+        return {'item': item}, 201
+        # return {"Message": "Item not found"}, 404
 
 
-# GET /store/<string:name>
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name :
-            return jsonify(store)
-    return jsonify({'message':"Store not found"})
+class ItemList(Resource):
+    def get(self):
+        return {"Item": items}
 
 
-# GET /store
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores':stores})
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-
-# POST /store/<string:name>/item {name:,price:}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_items_in_store(name):
-    request_data = request.get_json()
-    for store in stores : 
-        if store['name'] == name :
-            new_item = {
-                'name':request_data['name'],
-                'price': request_data['price']
-            }
-            stores['items'].append(new_item)
-            return jsonify(new_item)
-    return jsonify({'message':'Store not found'})
-
-
-# GET /store/<string:name>/item
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if  store == name:
-            return jsonify(store['items'])
-    return jsonify({"message":"Item in store not found"})
-
-
-app.run(port=4000)
+app.run(port=4000, debug=True)
